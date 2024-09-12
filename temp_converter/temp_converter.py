996e7d54080bbb96c78e54874bdb0e72b1369fef
dicziana <dac86@sunyorange.edu>
2024-09-12 11:51:47 -0400

    Update temp_converter.py

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
 # Convert Celsius to Fahrenheit
-def c2f(100):
-    return (cell * 9 / 5) + 32
+def c2f(cel):
+    # Correct Formula for celsius to Fahrenheit conversion
+    return c * 9 / 5 + 32
+    
 
-def main(cel):
-    Cel = 100
+def main():
+    Cel = 100 # example input for Celcius
+    print(c2f(cel)) # output the result for the conversion
 
 if __name__ == "__main__":
-    cel = 100         0
-    print(main(cel))  32
+    cel = 100         # input 
+    print(main(cel))  # output 
